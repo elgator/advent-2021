5ccef1049b3a6949d0ddce9e3c70da0a4d7b3def
elgator <pavel.s.sokolov@gmail.com>
2021-12-03 16:43:53 +0300

    day 03

---
+++ 03-1.jl
@@ -0,0 +1,79 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 39040d1e-5426-11ec-3b00-d1dedb8c652f
+parse_line(line) = split(line, "") .|> x-> ==(x, "1")
+
+# ╔═╡ 116d7455-af61-4197-a35b-9ecdce8c8c75
+report_test = readlines("./data/input3t.txt") .|> 
+	parse_line |> 
+	x -> foldl(hcat, x) |>
+	permutedims
+
+# ╔═╡ 1cabe137-bed9-42f1-a1e3-01d089e832dd
+function bitarr_to_int(arr, val = 0)
+    v = 2^(length(arr)-1)
+    for i in eachindex(arr)
+        val += v*arr[i]
+        v >>= 1
+    end
+    return val
+end
+
+# ╔═╡ 0753bd5c-220c-4af1-b8d4-0ab2c694b1ef
+function γ_ϵ(report)
+	one_counts = sum(report, dims=1)
+	zero_counts =  size(report)[1] .- one_counts
+	most = .~ .~ [o>z for (o,z) in zip(one_counts, zero_counts)]
+	least =  .~ .~ [o<z for (o,z) in zip(one_counts, zero_counts)]
+	γ = bitarr_to_int(most)
+	ϵ =  bitarr_to_int(least)
+	return γ, ϵ
+end
+
+# ╔═╡ b9c275ce-44a7-4ca5-bbf6-76bcc1a73bd6
+let
+	γ, ϵ = γ_ϵ(report_test)
+	@assert γ * ϵ == 198
+end
+
+# ╔═╡ 565baf5b-976a-47c2-945b-941cd8627b99
+report = readlines("./data/input3.txt") .|> 
+	parse_line |> 
+	x -> foldl(hcat, x) |>
+	permutedims
+
+# ╔═╡ da6e91a8-c0e1-48b5-a96f-cdb6df599e6f
+let
+	γ, ϵ = γ_ϵ(report)
+	ans1 = γ * ϵ
+end
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═39040d1e-5426-11ec-3b00-d1dedb8c652f
+# ╠═116d7455-af61-4197-a35b-9ecdce8c8c75
+# ╠═0753bd5c-220c-4af1-b8d4-0ab2c694b1ef
+# ╠═1cabe137-bed9-42f1-a1e3-01d089e832dd
+# ╠═b9c275ce-44a7-4ca5-bbf6-76bcc1a73bd6
+# ╠═565baf5b-976a-47c2-945b-941cd8627b99
+# ╠═da6e91a8-c0e1-48b5-a96f-cdb6df599e6f
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
